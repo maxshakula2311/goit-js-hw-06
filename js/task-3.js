8b10ae911579ef7b33808886dd61d62737d770ca
Max Shakula <maxshakulakr@gmail.com>
2025-11-30 23:58:09 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,28 @@
-function getElementWidth(content, padding, border) {
-    let contentWidth = (Number.parseFloat(content));
-    let paddingWidth = (Number.parseFloat(padding));
-    let borderWidth = (Number.parseFloat(border));
-    let totalWidth = (contentWidth + 2 * paddingWidth + 2 * borderWidth)
-    return totalWidth;
+class StringBuilder {
+    #value;
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+    getValue() {
+        return this.#value;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+    padEnd(str) {
+        this.#value = this.#value + str;
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
